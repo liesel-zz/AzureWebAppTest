05fbf6bb6e28aee5621488d418ae67688218a23f
Joel Backschat <joel@cafecomjava.com.br>
2018-03-01 20:50:11 -0300

    trocada a mensagem

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ appInsights.start();
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('E ai pessoal da dasa! '+process.env['MENSAGEM']);
+    res.end('E ai pessoal da dasaaaaaaaaaaaa! '+process.env['MENSAGEM']);
     
 }).listen(process.env.PORT || 8080);
