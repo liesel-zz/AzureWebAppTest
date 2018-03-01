07f0511d7aa65604c15a16ad934eca30f2cfcc96
Joel Backschat <joel@cafecomjava.com.br>
2018-03-01 20:29:05 -0300

    trocada a mensagem

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('E ai pessoal da dasa! '+process.env.MENSAGEM);
+    res.end('E ai pessoal da dasa! '+process.env['MENSAGEM']);
     
 }).listen(process.env.PORT || 8080);
