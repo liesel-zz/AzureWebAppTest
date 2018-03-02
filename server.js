069b24d8ceda91a26508e103333729b5aa5abb33
Joel Backschat <joel@cafecomjava.com.br>
2018-03-02 11:26:25 -0300

    trocada a mensagem

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 const appInsights = require("applicationinsights");
-appInsights.setup(process.env["APPINSIGHT"]);
+appInsights.setup(process.env['APPINSIGHT']);
 appInsights.start();
 
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end("Teste da apicacao"+process.env['MENSAGEM']);
+	res.end("Teste da aplicacao"+process.env['APPINSIGHT']);
     //res.end('E ai pessoal da dasaaaaaaaaaaaa! '+process.env['MENSAGEM']);
     
 }).listen(process.env.PORT || 8080);
