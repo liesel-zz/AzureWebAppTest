c3d3b81e4a0f9fc27cf122e77a212c2627817120
Joel Backschat <joel@cafecomjava.com.br>
2018-03-02 10:55:01 -0300

    trocada a mensagem

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.send("Teste da apicacao");
+	res.end("Teste da apicacao");
     //res.end('E ai pessoal da dasaaaaaaaaaaaa! '+process.env['MENSAGEM']);
     
 }).listen(process.env.PORT || 8080);
