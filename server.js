f3755ad6351e028e9330ed816ae83d684f3b1eff
Joel Backschat <joel@cafecomjava.com.br>
2018-03-13 14:00:16 -0300

    puxando a variavel do web app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end("Publicando direto do github");
+	res.end("Publicando direto do github"+process.env['MENSAGEM']);
     //res.end('E ai pessoal da dasaaaaaaaaaaaa! '+process.env['MENSAGEM']);
     
 }).listen(process.env.PORT || 8080);
