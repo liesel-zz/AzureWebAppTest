cd808235ba0e91b881339688c13307ec5c746620
Joel Backschat <joelbackschat@nbqsp-fc095.fcamara.com.br>
2018-05-23 11:02:24 -0300

    fé em Deus que vai

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-	// res.end("Publicando direto do github"+process.env['MENSAGEM']);
-    res.end('E ai pessoal da FCamara, desculpa pessoal .NET e melhor');
+	res.end("Publicando direto do github (com fe agora vai:)"+process.env['MENSAGEM']);
+    //res.end('E ai pessoal da FCamara, desculpa pessoal .NET e melhor');
     
-// }).listen(process.env.PORT || 8080);
-}).listen(3000);
+ }).listen(process.env.PORT || 8080);
+//}).listen(3000);
